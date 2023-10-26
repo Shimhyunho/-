e5b185cad9d4899192c26d6d9bd49babb99873db
Shimhyunho <shim2912@naver.com>
2023-10-27 02:08:11 +0900

    재수정

--- whyneoforet/js/common.js
+++ whyneoforet/js/common.js
@@ -1,13 +1,13 @@
 $(function(){
-    $('.text').css('display','none');
+    $('.text_wrap').css('display','none');
     $('.box').click(function(){
         // console.log('click!');
-        if($('.text').css('display') == 'none'){
-            $('.text').slideDown(300)
+        if($('.text_wrap').css('display') == 'none'){
+            $('.text_wrap').slideDown(300)
             $('.img').addClass('rotate');
         }
         else{
-            $('.text').slideUp(300);
+            $('.text_wrap').slideUp(300);
             $('.img').removeClass('rotate');
             $('.img').css('transition','.3s');
         }
